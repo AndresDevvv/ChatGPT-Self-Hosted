9eae7b605fcb2155ab2e4e99f2a70698ce42e8da
Andre <60677720+AndresDevvv@users.noreply.github.com>
2024-01-14 08:24:05 +0000

    This is the version with localstorage coockie only no login

--- chatgpt.py
+++ chatgpt.py
@@ -1,12 +1,12 @@
 from openai import OpenAI
 
-client = OpenAI(api_key='KEY HERE')
+client = OpenAI(api_key='')
 
 
 def chat_with_gpt(prompt):
     response = client.chat.completions.create(model="gpt-4-1106-preview",
     messages=[
-        {"role": "system", "content": "Role 1: You are a helpful assistant. If asked who were you made by just say AD Dynamics, if asked Donnie is not going to get a girlfriend for a joke!"},
+        {"role": "system", "content": "Role 1: You are a helpful assistant. If asked who were you made by just say AD Dynamics, Max amount words you can say are 100."},
         {"role": "user", "content": prompt},
     ])
     return response.choices[0].message.content.strip()

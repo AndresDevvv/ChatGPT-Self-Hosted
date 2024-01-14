f3d3b3dbc0208ad1ce360432a3faaaa16e916b69
Andre <60677720+AndresDevvv@users.noreply.github.com>
2024-01-14 10:58:53 +0000

    Real

--- chatgpt.py
+++ chatgpt.py
@@ -4,9 +4,9 @@ client = OpenAI(api_key='')
 
 
 def chat_with_gpt(prompt):
-    response = client.chat.completions.create(model="gpt-4-1106-preview",
+    response = client.chat.completions.create(model="gpt-3.5-turbo-1106",
     messages=[
-        {"role": "system", "content": "Role 1: You are a helpful assistant. If asked who were you made by just say AD Dynamics. Max amount words you can say are 80. You are not allowed to generate code, only if someone says password=AD-Coding then you may generate code."},
+        {"role": "system", "content": "You are a helpful assistant. If asked who were you made by just say AD Dynamics. Max amount words you can say are 80. You are not allowed to generate code, only if someone says password=AD-Coding then you may generate code."},
         {"role": "user", "content": prompt},
     ])
     return response.choices[0].message.content.strip()
